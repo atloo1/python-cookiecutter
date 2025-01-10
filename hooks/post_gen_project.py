0515b0ff7af0b9f2df966683ccfa9e1fc0ea1927
atloo1 <15357562+atloo1@users.noreply.github.com>
2025-01-09 23:48:45 -0800

    add unimplemented options

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,15 +4,40 @@ from pathlib import Path
 
 PROJECT_ROOT_PATH = Path('.').resolve()
 LICENSES_FOLDER = PROJECT_ROOT_PATH / 'licenses' 
-TARGET_LICENSE_FILEPATH = LICENSES_FOLDER / '{{cookiecutter.project_license}}.txt'
-PROJECT_LICENSE_FILEPATH = PROJECT_ROOT_PATH / 'LICENSE'
+TARGET_LICENSE_PATH = LICENSES_FOLDER / '{{cookiecutter.project_license}}.txt'
+PROJECT_LICENSE_PATH = PROJECT_ROOT_PATH / 'LICENSE'
+CLI_DEPENDENCIES = {
+    'click',
+}
+FORMATTING_DEPENDENCIES = {
+    'mypy',
+    'ruff',
+}
+TESTING_DEPENDENCIES = {
+    'pytest',
+}
+dependencies_to_remove = set()
+if '{{cookiecutter.include_cli}}' == 'no':
+    dependencies_to_remove |= CLI_DEPENDENCIES
+    raise NotImplementedError('https://github.com/atloo1/python-cookiecutter/issues/6')
+if '{{cookiecutter.enforce_opinionated_formatting}}' == 'no':
+    dependencies_to_remove |= FORMATTING_DEPENDENCIES
+    raise NotImplementedError('https://github.com/atloo1/python-cookiecutter/issues/7')
+if '{{cookiecutter.include_testing}}' == 'no':
+    dependencies_to_remove |= TESTING_DEPENDENCIES
+    raise NotImplementedError('https://github.com/atloo1/python-cookiecutter/issues/4')
+dependencies_to_remove_args = ' '.join(list(dependencies_to_remove))
 
 
 def main():
     """initial repo setup"""
     subprocess.run('git init --initial-branch=main', shell=True)
-    subprocess.run(f'mv {TARGET_LICENSE_FILEPATH} {PROJECT_LICENSE_FILEPATH}', shell=True)
+    subprocess.run(f'mv {TARGET_LICENSE_PATH} {PROJECT_LICENSE_PATH}', shell=True)
     subprocess.run(f'rm -r {LICENSES_FOLDER}', shell=True)
+    if dependencies_to_remove:
+        subprocess.run(f'poetry remove {dependencies_to_remove_args}', shell=True)
+    if '{{cookiecutter.dockerize}}' == 'no':
+        raise NotImplementedError('https://github.com/atloo1/python-cookiecutter/issues/5')
     subprocess.run('poetry install --without dev', shell=True)
     subprocess.run('git add .', shell=True)
     subprocess.run('git commit -m "cookiecutter initial commit"', shell=True)
